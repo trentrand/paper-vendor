dfbc6b7411c926afb9662522fab777b7e82d3e87
Trent Rand <contact@trentrand.com>
2020-10-21 18:47:17 -0400

    Add example task to blink led
    To ensure my board is correctly setup with FreeRTOS, I created a simple
    task which is responsible for blinking an led attached to gpio pin 16.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
+#include <stdlib.h>
 #include "espressif/esp_common.h"
-#include "FreeRTOS.h"
 #include "esp/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "esp8266.h"
+
+const int gpio = 16;
+
+void printTask(void *pvParameters) {
+  gpio_enable(gpio, GPIO_OUTPUT);
+
+  while(1) {
+    printf("Executing task\n");
+    gpio_write(gpio, 1);
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    gpio_write(gpio, 0);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
 void user_init(void) {
   uart_set_baud(0, 115200);
   printf("SDK version:%s\n", sdk_system_get_sdk_version());
+
+  xTaskCreate(printTask, "printTask", 256, NULL, 2, NULL);
 }
