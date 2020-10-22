56b65a670a7da8093ab78342a3c86d551bd232e5
Trent Rand <contact@trentrand.com>
2020-10-21 23:12:39 -0400

    Poll configuration of dispensable options from 4-bit DIP switch
    The dispensable options will be configurable by toggling their
    corresponding DIP switch.
    
    To get things started, I am simply polling this configuration and
    printing it out. Further learning might prove that this should be an
    interrupt; otherwise it's probably best to just read this once on boot.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,50 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "esp8266.h"
 
-const int gpio = 16;
+uint8_t const led_pin = 16;
 
-void printTask(void *pvParameters) {
-  gpio_enable(gpio, GPIO_OUTPUT);
+uint8_t const configuration_a_pin = 5;
+uint8_t const configuration_b_pin = 4;
+uint8_t const configuration_c_pin = 0;
+uint8_t const configuration_d_pin = 2;
+
+void printConfigurationGroup(char* identifier, bool value) {
+  char* readableValue = value ? "ON" : "OFF";
+  printf("%s = %s\t\t", identifier, readableValue);
+}
+
+void readConfiguration() {
+  bool configurationA = !gpio_read(configuration_a_pin);
+  bool configurationB = !gpio_read(configuration_b_pin);
+  bool configurationC = !gpio_read(configuration_c_pin);
+  bool configurationD = !gpio_read(configuration_d_pin);
+
+  printConfigurationGroup("A", configurationA);
+  printConfigurationGroup("B", configurationB);
+  printConfigurationGroup("C", configurationC);
+  printConfigurationGroup("D", configurationD);
+  printf("\n");
+}
+
+void pollConfigurationTask(void *pvParameters) {
+  gpio_enable(led_pin, GPIO_OUTPUT);
+  gpio_set_pullup(configuration_a_pin, true, false);
+  gpio_set_pullup(configuration_b_pin, true, false);
+  gpio_set_pullup(configuration_c_pin, true, false);
+  gpio_set_pullup(configuration_d_pin, true, false);
 
   while(1) {
-    printf("Executing task\n");
-    gpio_write(gpio, 1);
-    vTaskDelay(10000 / portTICK_PERIOD_MS);
-    gpio_write(gpio, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    gpio_write(led_pin, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    gpio_write(led_pin, 1);
+
+    readConfiguration();
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -23,5 +52,5 @@ void user_init(void) {
   uart_set_baud(0, 115200);
   printf("SDK version:%s\n", sdk_system_get_sdk_version());
 
-  xTaskCreate(printTask, "printTask", 256, NULL, 2, NULL);
+  xTaskCreate(pollConfigurationTask, "Poll Configuration", 256, NULL, 2, NULL);
 }
